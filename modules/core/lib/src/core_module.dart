f939565a3e4f0a2ae27f655111887927c7c257ea
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-09-11 13:07:37 +0700

    feat(core): define `BaseModule` interface and scaffold `CoreModule`

--- modules/core/lib/src/core_module.dart
+++ modules/core/lib/src/core_module.dart
@@ -1,1 +1,8 @@
+import 'package:core/src/module/base_module.dart';
 
+class CoreModule extends BaseModule {
+  @override
+  void setupDependencies() {
+    // Setup core module dependencies
+  }
+}
---
+++ modules/core/lib/src/module/base_module.dart
@@ -0,0 +1,3 @@
+abstract class BaseModule {
+  void setupDependencies();
+}
